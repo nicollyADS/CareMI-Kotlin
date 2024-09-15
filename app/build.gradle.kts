5e03032329a6a8dddd8cf0656fb458bec8d7de6c
NicollyADS <adm.nicolly@gmail.com>
2024-09-15 02:51:32 -0300

    configurando o android manifest e dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,10 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+    // https://mvnrepository.com/artifact/com.squareup.okhttp3/okhttp
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    // https://mvnrepository.com/artifact/com.google.code.gson/gson
+    implementation("com.google.code.gson:gson:2.11.0")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
